550bbef972820163ab698bd87baa36aefcd61848
Marco Metz <mm@tpwd.de>
2023-04-06 17:22:28 +0200

    Redirect back zu new cms
    after changing password redirect to cms
    
    BBNAV-218

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,8 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {
-    sessions: "users/sessions"
+    sessions: "users/sessions",
+    passwords: "users/passwords"
   }
 
   get "user" => "users/status#show"
